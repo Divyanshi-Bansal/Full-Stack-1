2b77f161da0112a66faa99481c630578dcee2e3d
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-11-01 16:31:48 +0530

    make counter

--- JavaScript/Practical-15.js
+++ JavaScript/Practical-15.js
@@ -4,13 +4,16 @@ var plus = document.getElementById('plus');
 minus.addEventListener('click',decrease);
 plus.addEventListener('click',increase);
 
+var count =0;
 function decrease(){
     plus.removeEventListener('click',increase);
     minus.style.background = 'darkgrey';
+    document.getElementById('counter').innerHTML = count--;
 }
 
 function increase(){
     minus.removeEventListener('click',decrease);
     plus.style.background = 'darkgrey';
     plus.style.fontWeight = 'bold';
+    document.getElementById('counter').innerHTML = count++;
 }
