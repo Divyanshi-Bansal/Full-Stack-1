7c82ccf4efe7767175863d61d91f4dd61dbbb6a4
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-11-01 17:12:29 +0530

    fixed some issues

--- JavaScript/Practical-15.js
+++ JavaScript/Practical-15.js
@@ -1,25 +1,36 @@
 var minus = document.getElementById('minus');
 var plus = document.getElementById('plus');
+var counter = document.getElementById('counter');
 
 minus.addEventListener('click',decrease);
 plus.addEventListener('click',increase);
 
 var count =0;
+
 function decrease(){
     if(count != 0){
         //plus.removeEventListener('click',increase);
         minus.style.background = 'black';
-        plus.style.background = 'darkgrey';
+        plus.style.background = 'grey';
         count = count -1;
-        document.getElementById('counter').innerHTML = count;
+        counter.innerHTML = count;
+    }
+    if(count == 0){
+        minus.style.background = 'rgb(187, 184, 184)';
+        minus.style.border = 'hidden';
     }
 }
 
 function increase(){
     //minus.removeEventListener('click',decrease);
-    minus.style.background = 'darkgrey';
+    minus.style.background = 'grey';
     plus.style.background = 'black';
     plus.style.fontWeight = 'bold';
     count = count +1;
-    document.getElementById('counter').innerHTML = count;
+    counter.innerHTML = count;
 }
+
+counter.onclick = function(){
+    counter.style.border = 'hidden';
+};
+ 
