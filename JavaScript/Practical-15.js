c9b359e6e0749e902e486d27ea4749d16d7fda3a
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-11-01 16:25:48 +0530

    create event handler

--- JavaScript/Practical-15.js
+++ JavaScript/Practical-15.js
@@ -1,9 +1,16 @@
 var minus = document.getElementById('minus');
 var plus = document.getElementById('plus');
 
-minus.onclick = decrease();
-plus.onclick = increase();
+minus.addEventListener('click',decrease);
+plus.addEventListener('click',increase);
 
 function decrease(){
-    
+    plus.removeEventListener('click',increase);
+    minus.style.background = 'darkgrey';
+}
+
+function increase(){
+    minus.removeEventListener('click',decrease);
+    plus.style.background = 'darkgrey';
+    plus.style.fontWeight = 'bold';
 }
