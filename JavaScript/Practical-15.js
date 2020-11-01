75b77e7157b624009f96a0ca7c910a4bad6fb804
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-11-01 16:42:05 +0530

    complete counter

--- JavaScript/Practical-15.js
+++ JavaScript/Practical-15.js
@@ -8,7 +8,8 @@ var count =0;
 function decrease(){
     if(count != 0){
         //plus.removeEventListener('click',increase);
-        minus.style.background = 'darkgrey';
+        minus.style.background = 'black';
+        plus.style.background = 'darkgrey';
         count = count -1;
         document.getElementById('counter').innerHTML = count;
     }
@@ -16,7 +17,8 @@ function decrease(){
 
 function increase(){
     //minus.removeEventListener('click',decrease);
-    plus.style.background = 'darkgrey';
+    minus.style.background = 'darkgrey';
+    plus.style.background = 'black';
     plus.style.fontWeight = 'bold';
     count = count +1;
     document.getElementById('counter').innerHTML = count;
