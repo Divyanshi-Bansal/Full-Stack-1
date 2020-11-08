9f2612a22c7b63087bab08db7ff1d43f0b4db596
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-11-08 16:46:25 +0530

    create dynamically  list

--- JavaScript/Practical-16.js
+++ JavaScript/Practical-16.js
@@ -1,3 +1,27 @@
 var todoInput = document.querySelector("todo-input");
-var todoButton = document.querySelector("todo-button")
-var todoList = document.querySelector("todo-List")
+var todoButton = document.querySelector("todo-button");
+var todoList = document.querySelector("todo-List");
+
+
+todoButton.onClick = create;
+
+function create(){
+    var newdiv = document.createElement("div");
+    newdiv.classList.add('todo');
+
+    var newLi = document.createElement('li');
+    newLi.classList.add('todo-item');
+    newdiv.appendChild(newLi);
+
+    var checkbtn = document.createElement('button');
+    checkbtn.classList.add('check-btn');
+    checkbtn.innerHTML = '<i class = "fa fa-check"></i>';
+    newdiv.appendChild(checkbtn);
+
+    var deletebtn = document.createElement('button');
+    deletebtn.classList.add('delete-btn');
+    deletebtn.innerHTML = '<i class="fa fa-trash"></i>';
+    newdiv.appendChild(deletebtn);
+
+
+}
