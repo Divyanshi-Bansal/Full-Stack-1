879a616dac65db70df1dc80b98e3ce26e06960e0
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-11-15 20:04:43 +0530

    save text func

--- JavaScript/Practical-17.js
+++ JavaScript/Practical-17.js
@@ -18,12 +18,36 @@ function digitalClock(){
     var text = document.getElementById('text');
     document.getElementById('mood').innerHTML = mood;
 
-    text.onclick = (e) => {
-        e.preventDefault();
-        text.innerHTML = text;
-        
-        text.style.opacity = '0.7';
+    // text.onclick = (e) => {
+    //     e.preventDefault();
+    //     text.innerHTML = text;
+    //     text.style.opacity = '0.7';
+    // }
+
+    text.addEventListener("keypress",setName);
+    text.addEventListener("blur",setName);
+    getName();
+
+    function setName(e){
+        if (e.type == 'keypress'){
+            if(e.keycode == 13){
+                localStorage.setItem("text",e.target.innerHTML);
+                text.blur;
+            }
+        }
+        else{
+            localStorage.setItem("text",e.target.innerHTML);
+        }
     }
+    function getName(){
+        if (localStorage.getItem('text') === null){
+            text.innerHTML = '[Enter Text]';
+        }
+        else{
+            text.innerHTML = localStorage.getItem('text');
+        }
+    }
+
 
     document.body.style.backgroundImage = hour>=4 & hour <=6 ? "URL('../image/earlyMorningimg.jpg')" : hour >=7 & hour <12 ? "URL('../image/morningimg.jpg')" : hour >=12 & hour <= 16 ?"URL('../image/afternooonimg.jpg')" : hour >= 17 & hour <= 18 ? "URL('../image/eveningimg.jpg')" : hour >18 & hour <=23 ? "URL('../image/nightImg.jpg')" : "URL('../image/lateNightimg.jpg')" ;
     if ( hour >18 & hour <=23 ){
