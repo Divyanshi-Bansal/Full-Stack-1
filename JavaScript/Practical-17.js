92ca7b3321234b88981a756fbc3aa55a4043b8e8
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-11-15 20:08:25 +0530

    getName func. calling

--- JavaScript/Practical-17.js
+++ JavaScript/Practical-17.js
@@ -10,14 +10,14 @@ function digitalClock(){
     let ampm = hour >= 12 ? 'PM' : 'AM';
     let greeting = hour >=4 & hour <12 ? 'Good Morning' : hour >=12 & hour <= 16 ? 'Good Afternoon' : hour >= 17 & hour <= 18 ? "Good Evening" : "Good Night";
     let mood = hour >=4 & hour <12 ? String.fromCodePoint(128519) : hour >=12 & hour <= 16 ? String.fromCodePoint(128513) : hour >= 17 & hour <= 18 ? String.fromCodePoint(129488) : hour >18 & hour <=23 ? String.fromCodePoint(128520) : String.fromCodePoint(128564) ;
-
+    
     document.getElementById('time').innerHTML = time;
     document.getElementById('ampm').innerHTML = ampm;
     document.getElementById('greeting').innerHTML = greeting;
     document.getElementById('today').innerHTML = today;
     var text = document.getElementById('text');
     document.getElementById('mood').innerHTML = mood;
-
+    getName();
     // text.onclick = (e) => {
     //     e.preventDefault();
     //     text.innerHTML = text;
@@ -26,7 +26,7 @@ function digitalClock(){
 
     text.addEventListener("keypress",setName);
     text.addEventListener("blur",setName);
-    getName();
+    
 
     function setName(e){
         if (e.type == 'keypress'){
