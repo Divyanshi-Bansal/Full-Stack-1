953023de10a1e16e9988b2937dc44a9df92f1c5f
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-11-15 11:49:19 +0530

    set time ,day and date

--- JavaScript/Practical-17.js
+++ JavaScript/Practical-17.js
@@ -1,5 +1,18 @@
 function digitalClock(){
+
     let date = new Date();
+
+    let hour = date.getHours();
+    let time = date.toString().slice(16, 24);
+    let today = date.toString().slice(0,15);
+    document.getElementById('time').innerHTML = time;
+    document.getElementById('today').innerHTML = today;
+
+
+
+
+
+
 }
 
 
