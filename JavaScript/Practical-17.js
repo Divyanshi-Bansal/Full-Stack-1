0669f598d4852224e7fca8459ef42fadda6a7d7c
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-11-15 11:52:06 +0530

    fetch amorpm

--- JavaScript/Practical-17.js
+++ JavaScript/Practical-17.js
@@ -5,14 +5,19 @@ function digitalClock(){
     let hour = date.getHours();
     let time = date.toString().slice(16, 24);
     let today = date.toString().slice(0,15);
+
+    let ampm = hour >= 12 ? 'PM' : 'AM';
+
+
     document.getElementById('time').innerHTML = time;
+    
     document.getElementById('today').innerHTML = today;
 
 
 
 
 
-
+    setTimeout(digitalClock,500);
 }
 
 
