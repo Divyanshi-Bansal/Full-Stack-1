9c396e6151a117cbb98824da20bb621f6587ca99
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-11-15 12:03:39 +0530

    set greetings

--- JavaScript/Practical-17.js
+++ JavaScript/Practical-17.js
@@ -3,14 +3,17 @@ function digitalClock(){
     let date = new Date();
 
     let hour = date.getHours();
+    console.log(hour);
     let time = date.toString().slice(16, 24);
     let today = date.toString().slice(0,15);
 
     let ampm = hour >= 12 ? 'PM' : 'AM';
+    let greeting = hour >=4 & hour <12 ? 'Good Morning' : hour >=12 & hour <= 16 ? 'Good Afternoon' : hour >= 17 & hour <= 18 ? "Good Evening" : "Good Night";
 
 
     document.getElementById('time').innerHTML = time;
     document.getElementById('ampm').innerHTML = ampm;
+    document.getElementById('greeting').innerHTML = greeting;
     document.getElementById('today').innerHTML = today;
 
 
