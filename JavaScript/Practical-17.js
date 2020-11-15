d6dadd8b1dfd1af316bd46dd0f833c05d7b624b4
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-11-15 16:12:19 +0530

    set  emoji height

--- JavaScript/Practical-17.js
+++ JavaScript/Practical-17.js
@@ -20,10 +20,13 @@ function digitalClock(){
 
     text.onclick = (e) => {
         e.preventDefault();
-        text.innerHTML = text;
-        
-        text.style.opacity = '0.5';
-
+        if (text == null){
+            text.innerHTML = 'Enter Text';
+        }
+        else{
+            text.innerHTML = text;
+        }
+        text.style.opacity = '0.7';
     }
 
     document.body.style.backgroundImage = hour>=4 & hour <=6 ? "URL('../image/earlyMorningimg.jpg')" : hour >=7 & hour <12 ? "URL('../image/morningimg.jpg')" : hour >=12 & hour <= 16 ? "URL('../image/afternoonimg.jpg')" : hour >= 17 & hour <= 18 ? "URL('../image/eveningimg.jpg')" : hour >18 & hour <=23 ? "URL('../image/nightImg.jpg')" : "URL('../image/lateNightimg.jpg')" ;
