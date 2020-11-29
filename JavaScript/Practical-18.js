a2c85a141ad9c3e7f30f7100d6003be6f6546dbc
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-11-30 01:59:24 +0530

    partially done

--- JavaScript/Practical-18.js
+++ JavaScript/Practical-18.js
@@ -15,30 +15,109 @@ form.addEventListener('submit',(e)=>{
 });
 
 function checkInput(){
+    if (username.value ===''){
+        username.style.borderBottomColor = "red";
+        small.innerHTML = "Field can't be blank";
+        small.style.visibility = 'visible';
+        wrong.style.visibility = "visible";
+    }
+    else if(username.value.match(/^[a-z0-9]+$/g)){
+        username.style.borderBottomColor =  'green';
+        right.style.visibility  = 'visible';
+    }
+    else{
+        username.style.borderBottomColor = "red";
+        small.innerHTML = "Username must contain small letters and no special symbols!";
+        small.style.visibility = 'visible';
+        wrong.style.visibility = "visible";
+    }
+    if(email.value ===''){
+        email.style.borderBottomColor = "red";
+        small.innerHTML = "Field can't be blank";
+        small.style.visibility = 'visible';
+        wrong.style.visibility = "visible";
+    }
+    else if(email.value.match(/@gmail.com$/gim)){
+        email.style.borderBottomColor =  'green';
+        right.style.visibility  = 'visible';
+    }
+    else{
+        email.style.borderBottomColor = "red";
+        small.innerHTML = "enter a valid email id";
+        small.style.visibility = 'visible';
+        wrong.style.visibility = "visible";
+    }
+    if(password1.value ===''){
+        password1.style.borderBottomColor = "red";
+        small.innerHTML = "Field can't be blank";
+        small.style.visibility = 'visible';
+        wrong.style.visibility = "visible";
+    }
+    else{
+        password1.style.borderBottomColor = 'green';
+        right.style.visibility='visible';
+    }
+    if(password2.value ===''){
+        password2.style.borderBottomColor = "red";
+        small.innerHTML = "Field can't be blank";
+        small.style.visibility = 'visible';
+        wrong.style.visibility = "visible";
+    }
+    else if(password2.value.match(/password1/g)){
+        password2.style.borderBottomColor = 'green';
+        right.style.visibility = 'visible';
+    }
+    else{
+        password2.style.borderBottomColor = "red";
+        small.innerHTML = "password can't match!";
+        small.style.visibility = 'visible';
+        wrong.style.visibility = "visible";
+    }
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     var usernameValue = username.value;
     var emailValue = email.value;
     var password1Value = password1.value;
     var password2Value = password2.value;
 
-    var list = ['usernameValue','emailValue','password1Value','password2Value'];
+    var list = ['username','email','password1','password2'];
     for (let index = 0; index < list.length; index++) {
         var input = list[index];
-        if(usernameValue ===""){
+        if(input.value ===""){
             showError(input);
         }
         else{
-            if(input.match(/^[a-z0-9]+$/g)){
+            if(input.value.match(/^[a-z0-9]+$/g)){
                 showSuccess(input);
             }
             else{
-                
+                small.innerHTML = "username must contain small letters and no special symbols!";
             }
         }
     } 
 }
 
-function showError(){
-    username.style.borderBottomColor = "red";
+function showError(msg){
+    msg.style.borderBottomColor = "red";
     small.innerHTML = "Field can't be blank";
     small.style.visibility = 'visible';
     wrong.style.visibility = "visible";
