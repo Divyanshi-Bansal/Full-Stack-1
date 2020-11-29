95a09818e03be08173edc8871e3b5eddc4885d11
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-11-29 10:40:28 +0530

    stop reloading the page

--- JavaScript/Practical-18.js
+++ JavaScript/Practical-18.js
@@ -4,3 +4,8 @@ var email = document.getElementById("email");
 var password1 =  document.getElementById("password1");
 var password2 =  document.getElementById("password2");
 var small =  document.getElementById("small");
+
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+});
