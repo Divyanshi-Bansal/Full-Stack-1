bc37544980cbee0d9eacda157064b97e68699abd
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-11-29 10:34:27 +0530

    done with selectors

---
+++ JavaScript/Practical-18.js
@@ -0,0 +1,6 @@
+var form =  document.getElementById("form");
+var username = document.getElementById("username");
+var email = document.getElementById("email");
+var password1 =  document.getElementById("password1");
+var password2 =  document.getElementById("password2");
+var small =  document.getElementById("small");
