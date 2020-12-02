6af485edc814abf576af6bcf0083e008a99addd1
Divyanshi1234 <divyanshi.bansal_cs18@gla.ac.in>
2020-12-02 19:56:02 +0530

    progress work

--- JavaScript/Practical-19.js
+++ JavaScript/Practical-19.js
@@ -7,20 +7,26 @@ var next2 = document.getElementById('next2');
 var back1 = document.getElementById('back1');
 var back2 = document.getElementById('back2');
 
+var progress = document.getElementById('progress');
+
 next1.onclick = function(){
     form1.style.left = '-500px';
     form2.style.left = '40px';
+    progress.style.width = '236px';
 }
 next2.onclick = function(){
     form2.style.left = '-500px';
     form3.style.left = '40px';
+    progress.style.width = '100%';
 }
 
 back1.onclick = function(){
-    form1.style.left = '40px'
-    form2.style.left = '500px'
+    form1.style.left = '40px';
+    form2.style.left = '500px';
+    progress.style.width = '128px';
 }
 back2.onclick = function(){
     form2.style.left = '40px';
     form3.style.left = '500px';
+    progress.style.width = '236px';
 }
